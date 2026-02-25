568d9776f4753fb7d888a2a210a7c3325431ff2b
yanethvgzz-code <yanethvgzz@gmail.com>
2026-02-24 22:02:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask
+from flask import Flask, redner_templete, request, jsonify, make_response, session 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/detalle_pedido')
+def detalle_pedido():
+    import mysql.connector
+mydb = mysql.connector.connect(
+  host="46.28.42.226",
+  user="u760464709_24005242_usr",
+  password="u7?Jpkt>Y*E7",
+  database="u760464709_24005242_bd"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM detalle_pedido")
+myresult = mycursor.
+return male_repose(jsonify(myresult))
+
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO detalle_pedido (id_detalle, id_pedido, id_producto, cantidad, precio_unitario)"
+val = ("John", "Highway 21")
+mycursor.execute(sql, val)
+
+mydb.commit()
